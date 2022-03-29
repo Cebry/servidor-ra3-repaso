f70d5725e76f78d30a1c3829d4a53f846ae1a251
Javier Cebrián Muñoz <a20cemuja@iesgrancapitan.org>
2022-03-29 11:50:26 +0200

    fix: enlaces css

--- css.php
+++ css.php
@@ -1,5 +1,5 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="http://cpd.iesgrancapitan.org:9118/~cemuja/ra3//css/normalize.css">
-<link rel="stylesheet" href="http://cpd.iesgrancapitan.org:9118/~cemuja/ra3//css/styles.css">
+<link rel="stylesheet" href="http://cpd.iesgrancapitan.org:9118/~cemuja/ra3/css/normalize.css">
+<link rel="stylesheet" href="http://cpd.iesgrancapitan.org:9118/~cemuja/ra3/css/styles.css">
